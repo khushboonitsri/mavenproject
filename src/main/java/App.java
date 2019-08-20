e0d3ce19c7b71d27c73cd959d79159fbdc6d2746
khushboo sharma <khushboonitsri@gmail.com>
2019-08-20 16:47:07 +0530

    intial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,10 @@
+package com.amdocs;
+
+public class App
+{
+public static void main(String ar[])
+{
+System.out.println("hello khushboo");
+}
+}
+
---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+package com.amdocs;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class AppTest{
+
+@Test
+public void stringTest(){
+assertTrue(true);
+}
+}
